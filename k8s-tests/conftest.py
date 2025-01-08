21898f5ed13dde804693d7e7d594a2b400cebdce
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2025-01-08 05:23:11 +0000

    Change to use jinja2 to handle session.

--- k8s-tests/conftest.py
+++ k8s-tests/conftest.py
@@ -1,8 +1,33 @@
 import json
 import os
+import random
+from jinja2 import Environment
 import pytest
 import logging
 from names_generator import generate_name
+
+
+def random_name(seed: int = 0) -> str:
+    return generate_name(style="underscore", seed=seed).replace("_", "")
+
+
+def random_number(from_number: int, to_number: int) -> str:
+    return str(random.randint(from_number, to_number))
+
+
+func_dict = {
+    "student_id": lambda: "123456789",
+    "random_name": random_name,
+    "random_number": random_number
+}
+
+
+def render(template):
+    env = Environment()
+    jinja_template = env.from_string(template)
+    jinja_template.globals.update(func_dict)
+    template_string = jinja_template.render()
+    return template_string
 
 
 @pytest.fixture(scope='module', autouse=True)
@@ -22,17 +47,21 @@ def json_input(request):
         folder_path = folder_path.replace("/cleanup/", "/setup/")
         folder_path = folder_path.replace("/answer/", "/setup/")
         session_json_file = os.path.join(folder_path, "session.json")
+        test_name = os.path.splitext(os.path.basename(test_path_name))[0]
+        task_session_file = os.path.join(folder_path, f"{test_name}.json")
 
+        logging.info(task_session_file)
         if os.path.exists(session_json_file):
             with open(session_json_file, 'r', encoding="utf-8") as file:
                 session_json = json.load(file)
-                random_name = generate_name().replace("_", "")
-                student_id = "123456789"
+                if os.path.exists(task_session_file):
+                    with open(task_session_file, 'r', encoding="utf-8") as file1:
+                        task_session = json.load(file1)
+                        session_json.update(task_session)
+
                 for key, value in session_json.items():
                     if isinstance(value, str):
-                        session_json[key] = value \
-                            .replace("{random_name}", random_name) \
-                            .replace("{student_id}", student_id)
+                        session_json[key] = render(value)
                 logging.info(session_json)
             result.update(session_json)
         return result
