6220731ec7110cec0cb9fb681e6a51f67f82d3d6
ITHangle <143982227+ITHangle@users.noreply.github.com>
2025-01-19 11:24:41 +0000

    just test can show Q3

---
+++ tests/game01/03_create_pod_and_add_80_port/test_06_cleanup.py
@@ -0,0 +1,18 @@
+import logging
+
+import pytest
+
+from tests.helper.kubectrl_helper import build_kube_config, run_kubectl_command
+
+
+class TestCleanup:
+
+    @pytest.mark.order(6)
+    def test_cleanup(self, json_input):
+        kube_config = build_kube_config(
+            json_input["cert_file"], json_input["key_file"], json_input["host"]
+        )
+        namespace = json_input["namespace"]
+        command = f"kubectl delete namespace {namespace}"
+        result = run_kubectl_command(kube_config, command)
+        logging.info(result)
