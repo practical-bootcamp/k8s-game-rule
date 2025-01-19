466ffe04986250c15b5d575974fbb9d633824ec9
ITHangle <143982227+ITHangle@users.noreply.github.com>
2025-01-19 12:16:28 +0000

    test

--- tests/game01/03_create_pod_and_add_80_port/test_06_cleanup.py
+++ tests/game01/03_create_pod_and_add_80_port/test_06_cleanup.py
@@ -9,10 +9,9 @@ class TestCleanup:
 
     @pytest.mark.order(6)
     def test_cleanup(self, json_input):
-        kube_config = build_kube_config(
-            json_input["cert_file"], json_input["key_file"], json_input["host"]
-        )
-        namespace = json_input["namespace"]
-        command = f"kubectl delete namespace {namespace}"
+        kube_config = build_kube_config(json_input["cert_file"], json_input["key_file"], json_input["host"])
+        pod_name = "nginx"
+        pod_namespace = "default"
+        command = f"kubectl delete pod {pod_name} -n {pod_namespace}"
         result = run_kubectl_command(kube_config, command)
         logging.info(result)
