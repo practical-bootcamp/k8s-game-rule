92b1d429cfd3448f61c55aa0f34df7ed0570d7f7
ITHangle <143982227+ITHangle@users.noreply.github.com>
2025-01-19 15:57:28 +0000

    D

--- tests/game01/03_create_pod_and_add_80_port/test_06_cleanup.py
+++ tests/game01/03_create_pod_and_add_80_port/test_06_cleanup.py
@@ -3,16 +3,36 @@ import logging
 import pytest
 
 from tests.helper.kubectrl_helper import build_kube_config, run_kubectl_command
+from tests.helper.k8s_client_helper import configure_k8s_client
 
 
+@pytest.mark.order(6)
 class TestCleanup:
+    def test_delete_pod_with_library(self, json_input):
+        logging.debug(json_input)
+        k8s_client = configure_k8s_client(json_input)
+        pod_name = "nginx"
+        pod_namespace = "default"
 
-    @pytest.mark.order(6)
-    def test_cleanup(self, json_input):
+        # 删除指定的 Pod
+        response = k8s_client.delete_namespaced_pod(
+            name=pod_name,
+            namespace=pod_namespace,
+            body={}
+        )
+        logging.info(response)
+        assert response.status == "Success", f"Failed to delete Pod '{pod_name}' in namespace '{pod_namespace}'"
+
+    def test_delete_pod_with_kubectl(self, json_input):
         kube_config = build_kube_config(
             json_input["cert_file"], json_input["key_file"], json_input["host"]
         )
-        namespace = json_input["namespace"]
-        command = f"kubectl delete namespace {namespace}"
+        pod_name = "nginx"
+        pod_namespace = "default"
+
+        # 使用 kubectl 命令删除 Pod
+        command = f"kubectl delete pod {pod_name} -n {pod_namespace}"
         result = run_kubectl_command(kube_config, command)
         logging.info(result)
+
+        assert f"pod \"{pod_name}\" deleted" in result, f"Pod '{pod_name}' was not deleted successfully"
