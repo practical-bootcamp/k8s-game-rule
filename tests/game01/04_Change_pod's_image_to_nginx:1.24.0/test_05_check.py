ab64f4b2643d82ea1a42f3ef064860a3b2b622c2
ITHangle <143982227+ITHangle@users.noreply.github.com>
2025-01-20 08:07:20 +0000

    test

--- tests/game01/04_Change_pod's_image_to_nginx:1.24.0/test_05_check.py
+++ tests/game01/04_Change_pod's_image_to_nginx:1.24.0/test_05_check.py
@@ -6,15 +6,27 @@ from tests.helper.kubectrl_helper import build_kube_config, run_kubectl_command
 @pytest.mark.order(5)
 class TestCheck:
 
-    def test_001_pod_attributes_with_library(self, json_input):
+    def test_001_pod_exists_with_library(self, json_input):
         logging.debug(json_input)
         k8s_client = configure_k8s_client(json_input)
         pod_name = "nginx"
         pod_namespace = "default"
-        pod = k8s_client.read_namespaced_pod(name=pod_name, namespace=pod_namespace)
-        
-        assert pod.spec.containers[0].image == "nginx:1.24.0", "Pod image version is not nginx:1.24.0"
-        assert pod.spec.containers[0].ports[0].container_port == 80, "Pod containerPort is not 80"
-        assert pod.metadata.namespace == "default", "Pod namespace is not default"
-        assert pod.metadata.name == "nginx", "Pod name is not nginx"
+        pods = k8s_client.list_namespaced_pod(namespace=pod_namespace)
+        pod_names = {pod.metadata.name: pod.spec.containers[0].image for pod in pods.items}
+        assert pod_name in pod_names, f"Pod '{pod_name}' does not exist in namespace '{pod_namespace}'"
+        assert pod_names[pod_name] == "nginx:1.24.0", f"Pod '{pod_name}' does not have image 'nginx:1.24.0'"
 
+    def test_002_pod_exists_with_kubectl(self, json_input):
+        kube_config = build_kube_config(
+            json_input["cert_file"], json_input["key_file"], json_input["host"]
+        )
+        command = "kubectl get pod nginx -n default -o=json"
+        result = run_kubectl_command(kube_config, command)
+        logging.info(result)
+        pod_info = json.loads(result)
+        
+        pod_name_result = pod_info["metadata"]["name"]
+        pod_image_result = pod_info["spec"]["containers"][0]["image"]
+        
+        assert pod_name_result == "nginx", f"Pod '{pod_name}' does not exist in namespace 'default'"
+        assert pod_image_result == "nginx:1.24.0", f"Pod '{pod_name}' does not have image 'nginx:1.24.0'"
--- tests/game01/04_Change_pod's_image_to_nginx:1.24.0/test_06_cleanup.py
+++ tests/game01/04_Change_pod's_image_to_nginx:1.24.0/test_06_cleanup.py
@@ -1,6 +1,9 @@
 import logging
+
 import pytest
+
 from tests.helper.kubectrl_helper import build_kube_config, run_kubectl_command
+
 
 class TestCleanup:
 
@@ -14,15 +17,11 @@ class TestCleanup:
         pod_namespace = "default"
         pod_name = "nginx"
         
-        # 恢复 Pod 的图像为默认版本，在这里假设默认版本是 nginx:1.23.0
-        command = f"kubectl set image pod/{pod_name} nginx=nginx:1.23.0 -n {pod_namespace}"
+        # 删除 Pod 的命令 
+        command = f"kubectl delete pod {pod_name} -n {pod_namespace}"
+        
+        # 运行命令并记录结果
         result = run_kubectl_command(kube_config, command)
         
         logging.info(result)
-        assert "pod/nginx image updated" in result.lower(), f"Failed to reset image of Pod '{pod_name}' in namespace '{pod_namespace}'"
-        
-        # 检查并记录结果
-        command = f"kubectl get pod {pod_name} -n {pod_namespace} -o json"
-        result = run_kubectl_command(kube_config, command)
-        logging.info(result)
-        assert "nginx:1.23.0" in result, f"Pod '{pod_name}' image not reset to default in namespace '{pod_namespace}'"
+        assert "deleted" in result.lower(), f"Failed to delete Pod '{pod_name}' in namespace '{pod_namespace}'"
