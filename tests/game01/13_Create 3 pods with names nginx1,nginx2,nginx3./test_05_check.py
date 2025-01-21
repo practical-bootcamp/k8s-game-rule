319592116952e52609a13d713748f8b979830238
etg227 <144003800+etg227@users.noreply.github.com>
2025-01-21 16:23:39 +0000

    try

--- tests/game01/13_Create 3 pods with names nginx1,nginx2,nginx3./test_05_check.py
+++ tests/game01/13_Create 3 pods with names nginx1,nginx2,nginx3./test_05_check.py
@@ -1,45 +1,74 @@
-# test_05_check.py
-import asyncio
+import subprocess
 import logging
 import pytest
 import json
 from tests.helper.k8s_client_helper import configure_k8s_client
 from tests.helper.kubectrl_helper import build_kube_config, run_kubectl_command
 
-async def list_pods(k8s_client, namespace):
-    return k8s_client.list_namespaced_pod(namespace=namespace)
-
 @pytest.mark.order(5)
-@pytest.mark.asyncio
 class TestCheck:
-    async def test_001_pods_exist_with_library(self, json_input):
+    
+    def create_pod(self, k8s_client, pod_name):
+        pod_manifest = {
+            'apiVersion': 'v1',
+            'kind': 'Pod',
+            'metadata': {
+                'name': pod_name,
+                'labels': {
+                    'app': 'v1'
+                }
+            },
+            'spec': {
+                'containers': [{
+                    'name': 'nginx',
+                    'image': 'nginx:1.24.0',
+                    'ports': [{'containerPort': 80}]
+                }]
+            }
+        }
+        resp = k8s_client.create_namespaced_pod(
+            body=pod_manifest,
+            namespace='default')
+        logging.info(f"Pod created: {resp.metadata.name}")
+    
+    def test_001_pod_attributes_with_library(self, json_input):
+        logging.debug(json_input)
         k8s_client = configure_k8s_client(json_input)
+
         pod_names = ["nginx1", "nginx2", "nginx3"]
-        pod_namespace = json_input["namespace"]
-
-        pods = await list_pods(k8s_client, pod_namespace)
-        pod_list = {pod.metadata.name: pod.metadata.labels for pod in pods.items}
-
         for pod_name in pod_names:
-            assert pod_name in pod_list, f"Pod '{pod_name}' does not exist in namespace '{pod_namespace}'"
-            assert pod_list[pod_name]["app"] == "v1", f"Pod '{pod_name}' does not have label 'app=v1'"
+            self.create_pod(k8s_client, pod_name)
+        
+        for pod_name in pod_names:
+            pod = k8s_client.read_namespaced_pod(name=pod_name, namespace='default')
+            assert pod.spec.containers[0].image == "nginx:1.24.0", "Pod image version is not nginx:1.24.0"
+            assert pod.spec.containers[0].ports[0].container_port == 80, "Pod containerPort is not 80"
+            assert pod.metadata.namespace == "default", "Pod namespace is not default"
+            assert pod.metadata.name == pod_name, "Pod name is not {}".format(pod_name)
+            assert pod.metadata.labels["app"] == "v1", "Pod label app is not v1"
 
-    async def test_002_pods_exist_with_kubectl(self, json_input):
+    def test_002_pod_attributes_with_kubectl(self, json_input):
         kube_config = build_kube_config(
-            json_input["cert_file"], 
-            json_input["key_file"],
-            json_input["host"]
+            json_input["$client_certificate"],
+            json_input["$client_key"],
+            json_input["$endpoint"],
         )
-        command = "kubectl get pods -n default -o json"
-        result = await asyncio.to_thread(run_kubectl_command, kube_config, command)
-        logging.info(result)
-        
-        pod_data = json.loads(result)
-        pod_names = ["nginx1", "nginx2", "nginx3"]
 
-        for pod in pod_data["items"]:
-            if pod["metadata"]["name"] in pod_names:
-                pod_names.remove(pod["metadata"]["name"])
-                assert pod["metadata"]["labels"]["app"] == "v1", f"Pod '{pod['metadata']['name']}' does not have label 'app=v1'"
-        
-        assert not pod_names, "Some of the required Pods are missing"
+        pod_names = ["nginx1", "nginx2", "nginx3"]
+        for pod_name in pod_names:
+            command = f"kubectl get pod {pod_name} -n default -o json"
+            result = subprocess.run(command, shell=True, capture_output=True, text=True)
+            
+            if result.returncode != 0:
+                logging.error(f"Command failed with error: {result.stderr}")
+            else:
+                json_output = result.stdout.strip()
+                logging.info(json_output)
+            
+                pod_data = json.loads(json_output)
+                
+                assert pod_data["spec"]["containers"][0]["image"] == "nginx:1.24.0", "Pod image version is not nginx:1.24.0"
+                assert pod_data["spec"]["containers"][0]["ports"][0]["containerPort"] == 80, "Pod containerPort is not 80"
+                assert pod_data["metadata"]["namespace"] == "default", "Pod namespace is not default"
+                assert pod_data["metadata"]["name"] == pod_name, f"Pod name is not {pod_name}"
+                assert pod_data["metadata"]["labels"]["app"] == "v1", "Pod label app is not v1"
