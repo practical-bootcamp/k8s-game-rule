4eca042f77f7028cdee3eae84d59cb0ec18a5cd3
etg227 <144003800+etg227@users.noreply.github.com>
2025-01-21 15:48:20 +0000

    fix

--- tests/game01/04_Change_pod's_image_to_nginx:1.24.0/test_05_check.py
+++ tests/game01/04_Change_pod's_image_to_nginx:1.24.0/test_05_check.py
@@ -1,38 +1,42 @@
-# test_05_check.py
+import subprocess
 import logging
 import pytest
+import json
 from tests.helper.k8s_client_helper import configure_k8s_client
+from tests.helper.kubectrl_helper import build_kube_config, run_kubectl_command
 
 @pytest.mark.order(5)
 class TestCheck:
-    def test_001_pods_exist_with_library(self, json_input):
+    def test_001_pod_attributes_with_library(self, json_input):
+        logging.debug(json_input)
         k8s_client = configure_k8s_client(json_input)
-        pod_names = ["nginx1", "nginx2", "nginx3"]
-        pod_namespace = json_input["namespace"]
+        pod_name = "nginx"
+        pod_namespace = "default"
+        pod = k8s_client.read_namespaced_pod(name=pod_name, namespace=pod_namespace)
+        
+        assert pod.spec.containers[0].image == "nginx:1.24.0", "Pod image version is not nginx:1.24.0"
+        assert pod.spec.containers[0].ports[0].container_port == 80, "Pod containerPort is not 80"
+        assert pod.metadata.namespace == "default", "Pod namespace is not default"
+        assert pod.metadata.name == "nginx", "Pod name is not nginx"
 
-        pods = k8s_client.list_namespaced_pod(namespace=pod_namespace)
-        pod_list = {pod.metadata.name: pod.metadata.labels for pod in pods.items}
-
-        for pod_name in pod_names:
-            assert pod_name in pod_list, f"Pod '{pod_name}' does not exist in namespace '{pod_namespace}'"
-            assert pod_list[pod_name]["app"] == "v1", f"Pod '{pod_name}' does not have label 'app=v1'"
-
-    def test_002_pods_exist_with_kubectl(self, json_input):
+    def test_002_pod_attributes_with_kubectl(self, json_input):
         kube_config = build_kube_config(
-            json_input["cert_file"], 
-            json_input["key_file"],
-            json_input["host"]
+            json_input["$client_certificate"],
+            json_input["$client_key"],
+            json_input["$endpoint"],
         )
-        command = "kubectl get pods -n default -o json"
-        result = run_kubectl_command(kube_config, command)
-        logging.info(result)
+        command = "kubectl get pod nginx -n default -o json"
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
         
-        pod_data = json.loads(result)
-        pod_names = ["nginx1", "nginx2", "nginx3"]
-
-        for pod in pod_data["items"]:
-            if pod["metadata"]["name"] in pod_names:
-                pod_names.remove(pod["metadata"]["name"])
-                assert pod["metadata"]["labels"]["app"] == "v1", f"Pod '{pod['metadata']['name']}' does not have label 'app=v1'"
+        if result.returncode != 0:
+            logging.error(f"Command failed with error: {result.stderr}")
+        else:
+            json_output = result.stdout.strip()
+            logging.info(json_output)
         
-        assert not pod_names, "Some of the required Pods are missing"
+            pod_data = json.loads(json_output)
+            
+            assert pod_data["spec"]["containers"][0]["image"] == "nginx:1.24.0", "Pod image version is not nginx:1.24.0"
+            assert pod_data["spec"]["containers"][0]["ports"][0]["containerPort"] == 80, "Pod containerPort is not 80"
+            assert pod_data["metadata"]["namespace"] == "default", "Pod namespace is not default"
+            assert pod_data["metadata"]["name"] == "nginx", "Pod name is not nginx"
--- tests/game01/13_Create 3 pods with names nginx1,nginx2,nginx3./test_05_check.py
+++ tests/game01/13_Create 3 pods with names nginx1,nginx2,nginx3./test_05_check.py
@@ -1,5 +1,3 @@
-# test_05_check.py
-import subprocess
 import logging
 import pytest
 import json
@@ -11,7 +9,8 @@ class TestCheck:
     def test_001_pods_exist_with_library(self, json_input):
         k8s_client = configure_k8s_client(json_input)
         pod_names = ["nginx1", "nginx2", "nginx3"]
-        pod_namespace = "default"
+        pod_namespace = json_input["namespace"]
+
         pods = k8s_client.list_namespaced_pod(namespace=pod_namespace)
         pod_list = {pod.metadata.name: pod.metadata.labels for pod in pods.items}
 
@@ -21,7 +20,9 @@ class TestCheck:
 
     def test_002_pods_exist_with_kubectl(self, json_input):
         kube_config = build_kube_config(
-            json_input["cert_file"], json_input["key_file"], json_input["host"]
+            json_input["cert_file"], 
+            json_input["key_file"],
+            json_input["host"]
         )
         command = "kubectl get pods -n default -o json"
         result = run_kubectl_command(kube_config, command)
@@ -36,4 +37,3 @@ class TestCheck:
                 assert pod["metadata"]["labels"]["app"] == "v1", f"Pod '{pod['metadata']['name']}' does not have label 'app=v1'"
         
         assert not pod_names, "Some of the required Pods are missing"
-
