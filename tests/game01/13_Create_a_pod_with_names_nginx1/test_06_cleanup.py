2ed12d85ffc11f326fc035be016d80e2300256bc
etg227 <144003800+etg227@users.noreply.github.com>
2025-02-01 09:57:16 +0000

    fix d

--- tests/game01/13_Create_a_pod_with_names_nginx1/test_06_cleanup.py
+++ tests/game01/13_Create_a_pod_with_names_nginx1/test_06_cleanup.py
@@ -12,13 +12,14 @@ class TestCleanup:
         )
         
         pod_namespace = "default"
-        pod_name = "nginx1"
+        pod_names = ["nginx2", "nginx3", "nginx1"]
         
-        # 删除 Pod 的命令 
-        command = f"kubectl delete pod {pod_name} -n {pod_namespace}"
-        
-        # 运行命令并记录结果
-        result = run_kubectl_command(kube_config, command)
-        
-        logging.info(result)
-        assert "deleted" in result.lower(), f"Failed to delete Pod '{pod_name}' in namespace '{pod_namespace}'"
+        for pod_name in pod_names:
+            # 删除 Pod 的命令 
+            command = f"kubectl delete pod {pod_name} -n {pod_namespace}"
+            
+            # 运行命令并记录结果
+            result = run_kubectl_command(kube_config, command)
+            
+            logging.info(result)
+            assert "deleted" in result.lower(), f"Failed to delete Pod '{pod_name}' in namespace '{pod_namespace}'"
