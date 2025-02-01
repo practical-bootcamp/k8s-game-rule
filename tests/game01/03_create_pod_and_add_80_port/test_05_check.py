21215459dd1eaf55a1d452c772d031f739d1e497
etg227 <144003800+etg227@users.noreply.github.com>
2025-02-01 07:19:51 +0000

    fix

--- tests/game01/03_create_pod_and_add_80_port/test_05_check.py
+++ tests/game01/03_create_pod_and_add_80_port/test_05_check.py
@@ -1,5 +1,5 @@
 import logging
-
+import json
 import pytest
 from tests.helper.k8s_client_helper import configure_k8s_client
 from tests.helper.kubectrl_helper import build_kube_config, run_kubectl_command
@@ -17,12 +17,22 @@ class TestCheck:
         pod_names = [pod.metadata.name for pod in pods.items]
         assert pod_name in pod_names, f"Pod '{pod_name}' does not exist in namespace '{pod_namespace}'"
 
-    def test_002_pod_exists_with_kubectl(self, json_input):
+    def test_002_pod_attributes_with_kubectl(self, json_input):
         kube_config = build_kube_config(
-            json_input["cert_file"], json_input["key_file"], json_input["host"]
+            json_input["cert_file"],
+            json_input["key_file"],
+            json_input["host"]
         )
-        command = "kubectl get pods -n default"
-        result = run_kubectl_command(kube_config, command)
-        logging.info(result)
         pod_name = "nginx"
-        assert pod_name in result, f"Pod '{pod_name}' does not exist in namespace 'default'"
+        pod_namespace = "default"
+        command = f"kubectl get pod {pod_name} -n {pod_namespace} -o json"
+        try:
+            result = run_kubectl_command(kube_config, command)
+            logging.info(result)
+            pod_data = json.loads(result)
+            assert pod_data["spec"]["containers"][0]["image"] == "nginx:1.24.0", "Pod image version is not nginx:1.24.0"
+            assert pod_data["spec"]["containers"][0]["ports"][0]["containerPort"] == 80, "Pod containerPort is not 80"
+            assert pod_data["metadata"]["namespace"] == "default", "Pod namespace is not default"
+            assert pod_data["metadata"]["name"] == "nginx", "Pod name is not nginx"
+        except Exception as e:
+            logging.error(f"Failed to execute command '{command}': {e}")
