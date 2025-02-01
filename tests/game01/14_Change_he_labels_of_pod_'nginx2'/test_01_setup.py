c17dedad0e1f79a5c2e5f2b9458bccc73b4e81ea
etg227 <144003800+etg227@users.noreply.github.com>
2025-02-01 09:29:55 +0000

    fix

--- tests/game01/14_Change_he_labels_of_pod_'nginx2'/test_01_setup.py
+++ tests/game01/14_Change_he_labels_of_pod_'nginx2'/test_01_setup.py
@@ -1,4 +1,3 @@
-# test_01_setup.py
 import pytest
 import logging
 from tests.helper.k8s_client_helper import configure_k8s_client
@@ -10,22 +9,28 @@ def test_setup(json_input):
     pod_namespace = json_input["namespace"]
 
     for pod_name in pod_names:
-        pod_manifest = {
-            "apiVersion": "v1",
-            "kind": "Pod",
-            "metadata": {
-                "name": pod_name,
-                "labels": {
-                    "app": "v1"
+        try:
+            # 检查Pod是否已经存在
+            k8s_client.read_namespaced_pod(name=pod_name, namespace=pod_namespace)
+            logging.info(f"Pod '{pod_name}' already exists in namespace '{pod_namespace}'")
+        except Exception as e:
+            # 如果Pod不存在，则创建它
+            pod_manifest = {
+                "apiVersion": "v1",
+                "kind": "Pod",
+                "metadata": {
+                    "name": pod_name,
+                    "labels": {
+                        "app": "v1"
+                    },
+                    "namespace": pod_namespace
                 },
-                "namespace": pod_namespace
-            },
-            "spec": {
-                "containers": [{
-                    "name": "nginx",
-                    "image": "nginx:latest"
-                }]
+                "spec": {
+                    "containers": [{
+                        "name": "nginx",
+                        "image": "nginx:latest"
+                    }]
+                }
             }
-        }
-        response = k8s_client.create_namespaced_pod(namespace=pod_namespace, body=pod_manifest)
-        logging.info(f"Created Pod: {pod_name} response: {response}")
+            response = k8s_client.create_namespaced_pod(namespace=pod_namespace, body=pod_manifest)
+            logging.info(f"Created Pod: {pod_name} response: {response}")
