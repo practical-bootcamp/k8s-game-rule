e8bf9a5a5912e202689147fa41887882d19f9e51
etg227 <144003800+etg227@users.noreply.github.com>
2025-02-02 12:39:28 +0800

    Update test_05_check.py

--- tests/game01/15_Add_a_new_label_tier1/test_05_check.py
+++ tests/game01/15_Add_a_new_label_tier1/test_05_check.py
@@ -5,18 +5,18 @@ from tests.helper.k8s_client_helper import configure_k8s_client
 
 @pytest.mark.order(5)
 class TestCheck:
-    def test_001_add_new_label(self, json_input):
+    def test_001_check_label(self, json_input):
         k8s_client = configure_k8s_client(json_input)
         pod_namespace = json_input["namespace"]
         
         pod_names = ["nginx1", "nginx2", "nginx3"]
-        new_label = {"tier": "web"}
 
         for pod_name in pod_names:
             pod = k8s_client.read_namespaced_pod(name=pod_name, namespace=pod_namespace)
             if pod.metadata.labels["app"] in ["v1", "v2"]:
-                k8s_client.patch_namespaced_pod(name=pod_name, namespace=pod_namespace, body={"metadata": {"labels": new_label}})
-                logging.info(f"Updated Labels for Pod: {pod_name}")
+                assert "tier" in pod.metadata.labels and pod.metadata.labels["tier"] == "web", f"Pod '{pod_name}' does not have the label 'tier=web'"
+                logging.info(f"Pod '{pod_name}' has the label 'tier=web'")
+
 
     def test_002_verify_new_label(self, json_input):
         k8s_client = configure_k8s_client(json_input)
