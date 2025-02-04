5e6ffa2225f2aa2d8c1c5d49d00388dde50df9b7
HoChunSun <144003190+HoChunSun@users.noreply.github.com>
2025-02-04 10:05:57 +0000

    20

--- tests/game01/20_Create_pod_will_deploy_Node/test_01_setup.py
+++ tests/game01/20_Create_pod_will_deploy_Node/test_01_setup.py
@@ -3,25 +3,6 @@ import pytest
 import logging
 from tests.helper.k8s_client_helper import configure_k8s_client
 
-def create_pod_manifest(pod_name, label, namespace):
-    return {
-        "apiVersion": "v1",
-        "kind": "Pod",
-        "metadata": {
-            "name": pod_name,
-            "labels": {
-                "app": label
-            },
-            "namespace": namespace
-        },
-        "spec": {
-            "containers": [{
-                "name": "nginx",
-                "image": "nginx:latest"
-            }]
-        }
-    }
-
 def label_node(k8s_client, node_name, labels):
     body = {
         "metadata": {
@@ -36,7 +17,6 @@ def test_setup(json_input):
     pod_namespace = json_input["namespace"]
     node_name = "minikube"
 
-
     # 检查节点是否存在
     node = k8s_client.read_node(name=node_name)
     logging.info(f"Node '{node_name}' exists.")
@@ -47,14 +27,3 @@ def test_setup(json_input):
     }
     response = label_node(k8s_client, node_name, labels)
     logging.info(f"Labeled Node '{node_name}' with labels: {labels}, response: {response}")
-
-
-    pod_manifests = [
-        create_pod_manifest("nginx1", "v1", pod_namespace),
-        create_pod_manifest("nginx2", "v2", pod_namespace),
-        create_pod_manifest("nginx3", "v3", pod_namespace)
-    ]
-
-    for pod_manifest in pod_manifests:
-        response = k8s_client.create_namespaced_pod(namespace=pod_namespace, body=pod_manifest)
-        logging.info(f"Created Pod: {pod_manifest['metadata']['name']} response: {response}")
