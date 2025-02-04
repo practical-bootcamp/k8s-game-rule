a51196b4b9a98c5c00f065f4a673a893a9f23fb9
HoChunSun <144003190+HoChunSun@users.noreply.github.com>
2025-02-04 10:10:31 +0000

    20

--- tests/game01/20_Create_pod_will_deploy_Node/test_06_cleanup.py
+++ tests/game01/20_Create_pod_will_deploy_Node/test_06_cleanup.py
@@ -14,17 +14,16 @@ class TestCleanup:
         )
         
         pod_namespace = json_input["namespace"]
-        pod_names = ["nginx1", "nginx2", "nginx3", "gpu-pod"]
+        pod_name = "gpu-pod"
         
-        for pod_name in pod_names:
-            # 删除 Pod 的命令 
-            command = f"kubectl delete pod {pod_name} -n {pod_namespace}"
-            
-            # 运行命令并记录结果
-            result = run_kubectl_command(kube_config, command)
-            
-            logging.info(result)
-            assert "deleted" in result.lower(), f"Failed to delete Pod '{pod_name}' in namespace '{pod_namespace}'"
+        # 删除 Pod 的命令 
+        command = f"kubectl delete pod {pod_name} -n {pod_namespace}"
+        
+        # 运行命令并记录结果
+        result = run_kubectl_command(kube_config, command)
+        
+        logging.info(result)
+        assert "deleted" in result.lower(), f"Failed to delete Pod '{pod_name}' in namespace '{pod_namespace}'"
         
         # 删除节点标签
         k8s_client = configure_k8s_client(json_input)
