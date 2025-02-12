14ba50ed2977b8f8fca86a5fe8a1ff029577f5cd
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2025-02-12 00:42:11 +0000

    Refactor test_answer to use deploy_answer helper function

--- tests/game01/02_create_namespace/test_03_answer.py
+++ tests/game01/02_create_namespace/test_03_answer.py
@@ -1,30 +1,8 @@
-import logging
-import os
-
 import pytest
-from jinja2 import Environment
 
-from tests.helper.kubectrl_helper import build_kube_config, run_kubectl_command
+from tests.helper.test_helper import deploy_answer
 
 
 @pytest.mark.order(3)
 def test_answer(json_input):
-    kube_config = build_kube_config(
-        json_input["cert_file"], json_input["key_file"], json_input["host"]
-    )
-
-    current_folder = os.path.dirname(__file__)
-    template_path = os.path.join(current_folder, "answer.template.yaml")
-    yaml_path = os.path.join(current_folder, "answer.gen.yaml")
-
-    env = Environment()
-    jinja_template = env.from_string(open(template_path, "r", encoding="utf-8").read())
-
-    with open(template_path, "r", encoding="utf-8") as file:
-        yaml_content = jinja_template.render(json_input)
-        with open(yaml_path, "w", encoding="utf-8") as file:
-            file.write(yaml_content)
-
-    command = f"kubectl apply -f {yaml_path}"
-    result = run_kubectl_command(kube_config, command)
-    logging.info(result)
+    deploy_answer(json_input)
